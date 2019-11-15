3895684c66749b7196adac4dffed4d3bd0604847
ManiacalDr <56361874+ManiacalDr@users.noreply.github.com>
2019-11-15 00:45:46 -0600

    11/14

--- Code/main.cpp
+++ Code/main.cpp
@@ -53,7 +53,7 @@ struct Material {
 		refraction = vec4(r, g, b, i);
 	}
 
-	void print() {
+	void print() const{
 		printf("Material: \nDiffuse: %f, %f, %f\nSpecular: %f, %f, %f\nEmissivity: %f\n\n", diffuse.x, diffuse.y, diffuse.z, specular.x, specular.y, specular.z, emissivity);
 	}
 };
@@ -80,8 +80,9 @@ struct Object {
 		mat = *m;
 	}
 
-	void print() {
-		printf("Object: \nXform:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\nXfmi: \n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n", Xform[0][0], Xform[1][0], Xform[2][0], Xform[3][0], Xform[0][1], Xform[1][1], Xform[2][1], Xform[3][1], Xform[0][2], Xform[1][2], Xform[2][2], Xform[3][2], Xform[0][3], Xform[1][3], Xform[2][3], Xform[3][3]);
+	void print() const{
+		printf("Object: \nXform:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\nXfmi: \n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n", Xform[0][0], Xform[1][0], Xform[2][0], Xform[3][0], Xform[0][1], Xform[1][1], Xform[2][1], Xform[3][1], Xform[0][2], Xform[1][2], Xform[2][2], Xform[3][2], Xform[0][3], Xform[1][3], Xform[2][3], Xform[3][3],
+			Xfmi[0][0], Xfmi[1][0], Xfmi[2][0], Xfmi[3][0], Xfmi[0][1], Xfmi[1][1], Xfmi[2][1], Xfmi[3][1], Xfmi[0][2], Xfmi[1][2], Xfmi[2][2], Xfmi[3][2], Xfmi[0][3], Xfmi[1][3], Xfmi[2][3], Xfmi[3][3]);
 		mat.print();
 	}
 };
@@ -105,7 +106,7 @@ struct Ray { //Stores data for Ray
 		dir = vec4(glm::normalize(vec3(dir)), 0.0);
 	}
 
-	void print() {
+	void print() const{
 		printf("Ray:\nStart: %f, %f, %f\nDirection: %f, %f, %f\n", start.x, start.y, start.z, dir.x, dir.y, dir.z);
 		mat.print();
 	}
@@ -120,7 +121,7 @@ struct Light { //Stores our light data, its position in world space and the colo
 		color = vec3(r, g, b);
 	}
 
-	void print() {
+	void print() const{
 		printf("Light: \nPosition: %f, %f, %f\nColor: %f, %f, %f\n", position.x, position.y, position.z, color.x, color.y, color.z);
 	}
 };
@@ -160,7 +161,7 @@ vec3 PhongIllumination(const vec3 point,const Ray* ray,const Light* light,const 
 
 	//ray.print();
 	//object.print();
-	//light.print();
+	//light->print();
 
 	vec4 vertPos4 = object->Xfmi * vec4(point, 1.0);
 	vec3 vertPos = vec3(vertPos4) / vertPos4.w;
@@ -185,9 +186,9 @@ vec3 PhongIllumination(const vec3 point,const Ray* ray,const Light* light,const 
 
 bool Shadow(vec3 point,const Ray* ray,const Light* light) {
 	/*Check between the object and the light to see if this new ray intersects another object*/
-	Ray lightRay(point, light->position);
+	Ray lightRay(point, light->position - point);
 	vec3* test = closestIntersection(&lightRay);
-	if (test && (vectorMag(test) < 1.0))
+	if (test && vectorMag(test) < 1.0)
 		return true;
 	return false;
 };
@@ -197,18 +198,27 @@ Ray reflect(vec3* hitpoint, Ray* ray){
 	vec3 normal = normalize(*hitpoint);
 	return Ray(*hitpoint, vec3(ray->dir) - 2*dot(vec3(ray->dir), normal)*normal);
 };
-// Ray rafraction(vec3 point, Ray ray){//used to find the color that should be here if it is a rafractive object};
+
+Ray rafraction(vec3* hitpoint, Ray* ray){
+	/*used to find the color that should be here if it is a rafractive object*/
+	vec3 normal = normalize(*hitpoint);
+	double ni = dot(n, -r.d); 
+	double eta = current_index / new_index; 
+	return Ray((x, eta*ni - sqrt(1 - eta * eta*(1 - ni * ni)))*n + eta * r.d);
+	return Ray(*hitpoint, vec3(ray->dir) - 2 * dot(vec3(ray->dir), normal)*normal);
+};
 
 vec3 Shade(vec3 hitpoint, Ray* ray, Object* object) {
 	/*Used to discover what color a particular pixel should be*/
-	vec3 Color = vec3(0.0);
+	//std::cout << "Registered hit\n";
+	vec3 Color = backgroundColor;
 	for (const Light &light : lights){
-		if (!Shadow(point, ray, &light))
+		if (Shadow(hitpoint, ray, &light))
 			Color += PhongIllumination(hitpoint, ray, &light, object);
 	}
 	Ray space = reflect(&hitpoint, ray);
 	if (object->mat.emissivity == 0) Color += trace(&space);
-	//if refractive Color += trace(refraction(point, ray));
+	if refractive Color += trace(refraction(point, ray));
 	return normalize(Color);
 };
 
@@ -249,6 +259,7 @@ vec3* calcIntersection(vec3 u, vec3 v, vec3* ret){
 	
 	if (!t)
 		return NULL;
+	//std::cout << *t << " \n";
 	if (*t < 0.0)
 		return NULL;
 	
@@ -270,6 +281,7 @@ vec3* closestIntersection(Ray *ray, Object *ret) {// return the intersection poi
 
 	for (const Object &object : objects){
 		//print(object.Xfmi);
+		//object.print();
 		current = calcIntersection(vec3(object.Xfmi * ray->start), vec3(ray->dir), &space);//we "downcast" these vectors so that we can take the proper dot product of them
 		if(current) {
 			if (!smallest) { //If this is the first time we intersect an object
@@ -279,7 +291,7 @@ vec3* closestIntersection(Ray *ray, Object *ret) {// return the intersection poi
 			else if (vectorMag(current) < vectorMag(smallest)) { //If the new intersection is less then the current stored, change the current
 				smallest = current;
 				*ret = object;
-				//std::cout << "Got here\n";
+				std::cout << "Got here\n";
 			}
 		}
 	}
@@ -380,12 +392,7 @@ void processInput() {
 
 			printf("%f %f %f\n", arr[0], arr[1], arr[2]);
 
-			if(ingroup)
-				stack.push_back(scale(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-			else {
-				groupobject.Xfmi *= inverse(scale(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-				stack.push_back(scale(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-			}
+			stack.push_back(scale(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
 
 			i += 3;
 		}
@@ -397,12 +404,7 @@ void processInput() {
 
 			printf("%f %f %f\n", arr[0], arr[1], arr[2]);
 
-			if(ingroup)
-				stack.push_back(translate(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-			else {
-				groupobject.Xfmi *= inverse(translate(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-				stack.push_back(translate(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
-			}
+			stack.push_back(translate(mat4(1.0), vec3(arr[0], arr[1], arr[2])));
 			
 			i += 3;
 		}
@@ -414,19 +416,24 @@ void processInput() {
 
 			printf("%f %f %f %f\n", arr[0], arr[1], arr[2], arr[3]);
 
-			groupobject.Xfmi *= inverse(rotate(mat4(1.0), arr[0], vec3(arr[1], arr[2], arr[3])));
 			stack.push_back(rotate(mat4(1.0), arr[0], vec3(arr[1], arr[2], arr[3])));
 
 			i += 4;
 		}
 		else if ("sphere" == *i) {
-			mat4 temp = groupobject.Xform;//were going to save this for a moment
-			groupobject.Xform = mat4(1.0);//set it to a new matrix
+			mat4 temp = groupobject.Xfmi;
+			groupobject.Xfmi = mat4(1.0);//set it to a new matrix
+			for (auto object = stack.begin(); object != stack.end(); object++) {//this multiples from clostest to the top
+				groupobject.Xfmi = groupobject.Xfmi * *object;
+			}
+			groupobject.Xfmi *= temp;
+			temp = groupobject.Xform;//were going to save this for a moment
+			groupobject.Xform = mat4(1.0);
 			for (auto object = stack.rbegin(); object != stack.rend(); object++) {//this multiples from clostest to the top
-				groupobject.Xform = groupobject.Xform * *object;
+				groupobject.Xform = groupobject.Xform * inverse(*object);
 			}
 			groupobject.Xform *= temp;
-
+			//groupobject.print();
 			stack.clear();//we clear because the object is already transformed, if we did't do this the next sphere would be transformed by what is in the stack again
 			objects.push_back(groupobject); //this needs to call some sort of new operator to allocate memory for the thingy
 			cout << "\n";
@@ -523,13 +530,13 @@ int main() {
 
 	//Pixels[n][n][3]
 	float** Pixels[n];
-	for (int i = 0; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		Pixels[i] = new float*[n];
-		for (int j = 0; j <= n; j++) {
+		for (int j = 0; j < n; j++) {
 			Pixels[i][j] = new float[3];
 		}
 	}
-	
+	std::cout << "Allocated Pixels array\n";
 
 	pw = 2 * d / n;
 	ph = 2 * d / n;
@@ -550,9 +557,9 @@ int main() {
 			//print(current.dir);
 			//print(&current.start);
 			vec3 Color = trace(&current);
-			Pixels[j][i][0] = Color.r;
-			Pixels[j][i][1] = Color.g;
-			Pixels[j][i][2] = Color.b;
+			Pixels[i][j][0] = Color.r;
+			Pixels[i][j][1] = Color.g;
+			Pixels[i][j][2] = Color.b;
 			count++;
 		}
 	}
@@ -565,22 +572,24 @@ int main() {
 	fprintf(picfile, "P6# %dx%d Raytracer output\n%d %d\n255\n",
 			n,n,n,n);
 	// For each pixel
-	for (int j = 0; j < n; j++) {// Y is flipped!
+	for (int j = n - 1; j >= 0; j--) {// Y is flipped!
 		for (int i = 0; i < n; i++) {
-			r = Pixels[i][j][0] * 255;
-			g = Pixels[i][j][1] * 255;
-			b = Pixels[i][j][2] * 255;
+			r = int(Pixels[i][j][0] * 255);
+			g = int(Pixels[i][j][1] * 255);
+			b = int(Pixels[i][j][2] * 255);
 			fprintf(picfile, "%c%c%c", r, g, b);
 			//if (i == Xsize - 1)//use to dertimaine if your actually writing to the file in the case of large files
 			//	std::cout << "row\n";
-			//std::cout << Pixels[i][j][0] << " " << Pixels[i][j][1] << " " << Pixels[i][j][2] << " ";
+			//std::cout << int(Pixels[i][j][0] * 255) << " " << int(Pixels[i][j][1] * 255) << " " << int(Pixels[i][j][2] * 255) << " \n";
 			// Remember though that this is a number between 0 and 255
 			// so might have to convert from 0-1.
 			count++;
 		}
 		//fprintf(picfile, "\n");
 	}
+
 	std::cout << "Sucessfully wrote to the PPM " << count << "\n";
 	fclose(picfile);
 	return 0;
 }
+
